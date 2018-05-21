8c2564a432d4ae0aa3def6bf596b48d70b566f5c
Victor Monterroso <vicmmonterroso@gmail.com>
2018-05-21 02:46:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function happyHolidays() {
   return "Happy holidays";
 }
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`;
+}
